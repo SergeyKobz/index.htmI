27b1d08835c8e4fa8ff750c92eb59d9214c47818
SergeyKobz <cringekiddos1@gmail.com>
2024-06-06 13:49:43 +0300

    Add files via upload

---
+++ style.css
@@ -0,0 +1,19 @@
+.color0 {
+    background-color: #FFFFFF;
+}
+
+.color1 {
+    background-color: #33333D;
+}
+
+.color2 {
+    background-color: #FFFFFF;
+}
+
+.color3 {
+    background-color: #f4f4f4;
+}
+
+.color4 {
+    background-color: #33333D;
+}
